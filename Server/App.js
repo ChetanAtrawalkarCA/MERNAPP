c1575b54cd610510b8edcf9acc30e58d9839472e
Chetan Atrawalkar <chetanatrawalkar1@gmail.com>
2021-04-05 17:03:54 +0530

    Add Some Functionality

--- Server/App.js
+++ Server/App.js
@@ -5,13 +5,13 @@ const app = express();
 
 dotenv.config({ path: "./config.env" });
 
-require("./db/conn");
+require("./DB/conn");
 // const User = require('./model/userSchema');
 
 app.use(express.json());
 
 // we link the router files to make our route easy
-app.use(require("./router/auth"));
+app.use(require("./Router/Auth"));
 
 const PORT = process.env.PORT;
 
@@ -24,6 +24,7 @@ const middleware = (req, res, next) => {
 // app.get('/', (req, res) => {
 //     res.send(`Hello world from the server app.js`);
 // });
+const Port = 3000;
 
 app.get("/about", middleware, (req, res) => {
   console.log(`Hello my About`);
@@ -42,6 +43,6 @@ app.get("/signup", (req, res) => {
   res.send(`Hello Registration world from the server`);
 });
 
-app.listen(PORT, () => {
+app.listen(Port, () => {
   console.log(`server is runnig at port no ${PORT}`);
 });
